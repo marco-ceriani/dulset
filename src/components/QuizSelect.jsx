61c471fb9a4276295084c68890a32a308927ea4d
Marco Ceriani <marco-ceriani@users.noreply.github.com>
2022-07-05 21:38:37 +0200

    fix quiz selection

--- src/components/QuizSelect.jsx
+++ src/components/QuizSelect.jsx
@@ -14,30 +14,30 @@ const QuizSelect = (props) => {
 
     const QuizTypeButton = (props) => {
         const { type, children } = props;
-        return <li onClick={() => openQuiz(type)}>
-            <button className="quiz-select-btn">{children}</button>
-        </li>
+        return <button className="quiz-select-btn" onClick={() => openQuiz(type)}>
+            {children}
+        </button>
     }
 
     return <div className="quiz-selection">
-        <div className="slider-container">
+        <div>
             <h3>Difficulty</h3>
             <input type="range" min="1" max="3"
                 aria-label="select difficulty"
                 value={difficulty}
                 onChange={(e) => setDifficulty(parseInt(e.target.value))} />
         </div>
-        <section className="options-group">
+        <section>
             <h3>Native Numbers</h3>
-            <ul>
+            <div className="options-group">
                 <QuizTypeButton type="native-to">1 ➔ 하나</QuizTypeButton>
-                <QuizTypeButton type="sino-to">1 ➔ 일</QuizTypeButton>
-            </ul>
-            <h3>Sino-Korean Numbers</h3>
-            <ul>
                 <QuizTypeButton type="native-from">하나 ➔ 1</QuizTypeButton>
+            </div>
+            <h3>Sino-Korean Numbers</h3>
+            <div className="options-group">
+                <QuizTypeButton type="sino-to">1 ➔ 일</QuizTypeButton>
                 <QuizTypeButton type="sino-from">일 ➔ 1</QuizTypeButton>
-            </ul>
+            </div>
         </section>
     </div>
 }
